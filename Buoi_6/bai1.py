a719083efb175737f7c4983bd3877f9a32f119d1
dang thao <pvtt66789@gmail.com>
2024-06-02 13:53:45 +0700

    ly thuyet buoi 6

--- Buoi_6/bai1.py
+++ Buoi_6/bai1.py
@@ -1,7 +1,23 @@
-print("Xin Chaof")
+print("Hoc_git")
 
-# tao file git luu lich su tao file den kho du lieu
-# 1. git init
-# ket noi voi kho du lieu GIT
+# Tạo file .git : Để lưu lịch sử/ kết nối với kho dữ liệu.
+# git init
+
+# Kết nối với kho dữ liệu git
+# git remote add origin https://github.com/NguyenCuong9402/hoc_python.git
+
+# kiểm tra có remote thành công không
+# git remote -v
 
 
+# Add vào kho lưu trữ tạm thời
+# git add .
+# Comment để đọc lại đ ã làm gì
+# git commit -v " comment"
+
+# Đẩy các commit len git
+# git push
+
+
+# Nếu push lần đầu tiên
+# git push --set-upstream origin master
